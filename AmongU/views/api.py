2c9307a8c7fd2ebd883c68ea8215fcea55e78c74
Laser Nite <lasernite@gmail.com>
2013-07-10 20:14:06 -0500

    fixed import json

--- AmongU/views/api.py
+++ AmongU/views/api.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from AmongU.models import *
 from django.views.decorators.csrf import csrf_exempt
-import simplejson as json
+import json as simplejson
 
 
 
